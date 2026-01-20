09583b843df7400e8d0d14f17273ed410410cd2a
AndersonBillss <anderson@thebillsfamily.com>
2026-01-19 23:37:27 -0700

    Instantiate a webgpu instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,102 +1,22 @@
-#include <iostream> 
-#include <GLFW/glfw3.h>
-#include <webgpu/webgpu_cpp.h>
-#include <webgpu/webgpu_cpp_print.h>
+#include <webgpu/webgpu.h>
+#include <iostream>
 
-#include <string>
+int main(int, char **)
+{
+  WGPUInstanceDescriptor desc = {};
+  desc.nextInChain = nullptr;
 
-wgpu::Instance instance;
-wgpu::Adapter adapter;
-wgpu::Device device;
+  WGPUInstance instance = wgpuCreateInstance(&desc);
 
-const uint32_t kWidth = 512;
-const uint32_t kHeight = 512;
-
-wgpu::Surface surface;
-wgpu::TextureFormat format;
-
-void ConfigureSurface() {
-  wgpu::SurfaceCapabilities capabilities;
-  surface.GetCapabilities(adapter, &capabilities);
-  format = capabilities.formats[0];
-  
-  wgpu::SurfaceConfiguration config;
-  config.device = device;
-  config.format = format;
-  config.width = kWidth;
-  config.height = kHeight;
-  surface.Configure(&config);
-}
-
-void InitGraphics() {
-  ConfigureSurface();
-}
-
-
-void init() {
-  static const auto kTimedWaitAny = wgpu::InstanceFeatureName::TimedWaitAny;
-  wgpu::InstanceDescriptor instanceDesc;
-  instanceDesc.requiredFeatureCount = 1;
-  instanceDesc.requiredFeatures = &kTimedWaitAny;
-  instance = wgpu::CreateInstance(&instanceDesc);
-
-  wgpu::Future f1 = instance.RequestAdapter(
-      nullptr, wgpu::CallbackMode::WaitAnyOnly,
-      [](wgpu::RequestAdapterStatus status, wgpu::Adapter a,
-         wgpu::StringView message) {
-        if (status != wgpu::RequestAdapterStatus::Success) {
-          std::cout << "RequestAdapter: " << message << "\n";
-          exit(0);
-        }
-        adapter = std::move(a);
-      });
-  instance.WaitAny(f1, UINT64_MAX);
-
-  wgpu::DeviceDescriptor desc{};
-  desc.SetUncapturedErrorCallback([](const wgpu::Device&,
-                                     wgpu::ErrorType errorType,
-                                     wgpu::StringView message) {
-    std::cout << "Error: " << errorType << " - message: " << message << "\n";
-  });
-
-  wgpu::Future f2 = adapter.RequestDevice(
-      &desc, wgpu::CallbackMode::WaitAnyOnly,
-      [](wgpu::RequestDeviceStatus status, wgpu::Device d,
-         wgpu::StringView message) {
-        if (status != wgpu::RequestDeviceStatus::Success) {
-          std::cout << "RequestDevice: " << message << "\n";
-          exit(0);
-        }
-        device = std::move(d);
-      });
-  instance.WaitAny(f2, UINT64_MAX);
-}
-
-void Render() {
-  // TODO: Render a triangle using WebGPU.
-}
-
-void Start() {
-  if (!glfwInit()) {
-    return;
+  if (!instance)
+  {
+    std::cerr << "Could not initialize WebGPU!" << std::endl;
+    return 1;
   }
 
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow* window =
-      glfwCreateWindow(kWidth, kHeight, "WebGPU window", nullptr, nullptr);
+  std::cout << "WGPU instance: " << instance << std::endl;
 
-  // surface = wgpu::glfw::CreateSurfaceForWindow(instance, window);
+  wgpuInstanceRelease(instance);
 
-  InitGraphics();
-
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
-    Render();
-    surface.Present();
-    instance.ProcessEvents();
-  }
-}
-
-int main() {
-  Start();
+  return 0;
 }
