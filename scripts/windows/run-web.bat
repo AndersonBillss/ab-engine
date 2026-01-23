c1318ea55101ef8631978b7a9c58e2d016c0e738
AndersonBillss <anderson@thebillsfamily.com>
2026-01-22 18:00:10 -0700

    Created shell for running on web

--- scripts/windows/run-web.bat
+++ scripts/windows/run-web.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call scripts/windows/build-web.bat
+call scripts/windows/build-web.bat || exit 1
 
 echo Running engine at http://localhost:8000/ab_engine.html
 call python -m http.server --directory ./build/web-engine 8000
