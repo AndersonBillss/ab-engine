ea8706c05d0b4074ae38d4053e10d2efa64c8f3a
AndersonBillss <anderson@thebillsfamily.com>
2026-01-30 23:11:05 -0700

    Print out all the features my GPU supports

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <webgpu/webgpu.h>
 #include <iostream>
+#include <unordered_map>
 #include "requestAdapter.hpp"
 
 void onAdapterRequested(WGPUAdapter adapter, void *pInstance)
@@ -50,6 +51,114 @@ int main(int, char **)
     std::cout << " - maxTextureDimension3D: " << supportedLimits.maxTextureDimension3D << std::endl;
     std::cout << " - maxTextureArrayLayers: " << supportedLimits.maxTextureArrayLayers << std::endl;
   }
+
+  std::unordered_map<WGPUFeatureName, std::string> featureToString = {
+      {WGPUFeatureName_CoreFeaturesAndLimits, "WGPUFeatureName_CoreFeaturesAndLimits"},
+      {WGPUFeatureName_DepthClipControl, "WGPUFeatureName_DepthClipControl"},
+      {WGPUFeatureName_Depth32FloatStencil8, "WGPUFeatureName_Depth32FloatStencil8"},
+      {WGPUFeatureName_TextureCompressionBC, "WGPUFeatureName_TextureCompressionBC"},
+      {WGPUFeatureName_TextureCompressionBCSliced3D, "WGPUFeatureName_TextureCompressionBCSliced3D"},
+      {WGPUFeatureName_TextureCompressionETC2, "WGPUFeatureName_TextureCompressionETC2"},
+      {WGPUFeatureName_TextureCompressionASTC, "WGPUFeatureName_TextureCompressionASTC"},
+      {WGPUFeatureName_TextureCompressionASTCSliced3D, "WGPUFeatureName_TextureCompressionASTCSliced3D"},
+      {WGPUFeatureName_TimestampQuery, "WGPUFeatureName_TimestampQuery"},
+      {WGPUFeatureName_IndirectFirstInstance, "WGPUFeatureName_IndirectFirstInstance"},
+      {WGPUFeatureName_ShaderF16, "WGPUFeatureName_ShaderF16"},
+      {WGPUFeatureName_RG11B10UfloatRenderable, "WGPUFeatureName_RG11B10UfloatRenderable"},
+      {WGPUFeatureName_BGRA8UnormStorage, "WGPUFeatureName_BGRA8UnormStorage"},
+      {WGPUFeatureName_Float32Filterable, "WGPUFeatureName_Float32Filterable"},
+      {WGPUFeatureName_Float32Blendable, "WGPUFeatureName_Float32Blendable"},
+      {WGPUFeatureName_ClipDistances, "WGPUFeatureName_ClipDistances"},
+      {WGPUFeatureName_DualSourceBlending, "WGPUFeatureName_DualSourceBlending"},
+      {WGPUFeatureName_TextureFormatsTier1, "WGPUFeatureName_TextureFormatsTier1"},
+      {WGPUFeatureName_TextureFormatsTier2, "WGPUFeatureName_TextureFormatsTier2"},
+      {WGPUFeatureName_PrimitiveIndex, "WGPUFeatureName_PrimitiveIndex"},
+      {WGPUFeatureName_Subgroups, "WGPUFeatureName_Subgroups"},
+
+#ifndef __EMSCRIPTEN__
+      // Native backend-specific features
+      {WGPUFeatureName_TextureComponentSwizzle, "WGPUFeatureName_TextureComponentSwizzle"},
+      {WGPUFeatureName_DawnInternalUsages, "WGPUFeatureName_DawnInternalUsages"},
+      {WGPUFeatureName_DawnMultiPlanarFormats, "WGPUFeatureName_DawnMultiPlanarFormats"},
+      {WGPUFeatureName_DawnNative, "WGPUFeatureName_DawnNative"},
+      {WGPUFeatureName_ChromiumExperimentalTimestampQueryInsidePasses, "WGPUFeatureName_ChromiumExperimentalTimestampQueryInsidePasses"},
+      {WGPUFeatureName_ImplicitDeviceSynchronization, "WGPUFeatureName_ImplicitDeviceSynchronization"},
+      {WGPUFeatureName_TransientAttachments, "WGPUFeatureName_TransientAttachments"},
+      {WGPUFeatureName_MSAARenderToSingleSampled, "WGPUFeatureName_MSAARenderToSingleSampled"},
+      {WGPUFeatureName_D3D11MultithreadProtected, "WGPUFeatureName_D3D11MultithreadProtected"},
+      {WGPUFeatureName_ANGLETextureSharing, "WGPUFeatureName_ANGLETextureSharing"},
+      {WGPUFeatureName_PixelLocalStorageCoherent, "WGPUFeatureName_PixelLocalStorageCoherent"},
+      {WGPUFeatureName_PixelLocalStorageNonCoherent, "WGPUFeatureName_PixelLocalStorageNonCoherent"},
+      {WGPUFeatureName_Unorm16TextureFormats, "WGPUFeatureName_Unorm16TextureFormats"},
+      {WGPUFeatureName_MultiPlanarFormatExtendedUsages, "WGPUFeatureName_MultiPlanarFormatExtendedUsages"},
+      {WGPUFeatureName_MultiPlanarFormatP010, "WGPUFeatureName_MultiPlanarFormatP010"},
+      {WGPUFeatureName_HostMappedPointer, "WGPUFeatureName_HostMappedPointer"},
+      {WGPUFeatureName_MultiPlanarRenderTargets, "WGPUFeatureName_MultiPlanarRenderTargets"},
+      {WGPUFeatureName_MultiPlanarFormatNv12a, "WGPUFeatureName_MultiPlanarFormatNv12a"},
+      {WGPUFeatureName_FramebufferFetch, "WGPUFeatureName_FramebufferFetch"},
+      {WGPUFeatureName_BufferMapExtendedUsages, "WGPUFeatureName_BufferMapExtendedUsages"},
+      {WGPUFeatureName_AdapterPropertiesMemoryHeaps, "WGPUFeatureName_AdapterPropertiesMemoryHeaps"},
+      {WGPUFeatureName_AdapterPropertiesD3D, "WGPUFeatureName_AdapterPropertiesD3D"},
+      {WGPUFeatureName_AdapterPropertiesVk, "WGPUFeatureName_AdapterPropertiesVk"},
+      {WGPUFeatureName_DawnFormatCapabilities, "WGPUFeatureName_DawnFormatCapabilities"},
+      {WGPUFeatureName_DawnDrmFormatCapabilities, "WGPUFeatureName_DawnDrmFormatCapabilities"},
+      {WGPUFeatureName_MultiPlanarFormatNv16, "WGPUFeatureName_MultiPlanarFormatNv16"},
+      {WGPUFeatureName_MultiPlanarFormatNv24, "WGPUFeatureName_MultiPlanarFormatNv24"},
+      {WGPUFeatureName_MultiPlanarFormatP210, "WGPUFeatureName_MultiPlanarFormatP210"},
+      {WGPUFeatureName_MultiPlanarFormatP410, "WGPUFeatureName_MultiPlanarFormatP410"},
+      {WGPUFeatureName_SharedTextureMemoryVkDedicatedAllocation, "WGPUFeatureName_SharedTextureMemoryVkDedicatedAllocation"},
+      {WGPUFeatureName_SharedTextureMemoryAHardwareBuffer, "WGPUFeatureName_SharedTextureMemoryAHardwareBuffer"},
+      {WGPUFeatureName_SharedTextureMemoryDmaBuf, "WGPUFeatureName_SharedTextureMemoryDmaBuf"},
+      {WGPUFeatureName_SharedTextureMemoryOpaqueFD, "WGPUFeatureName_SharedTextureMemoryOpaqueFD"},
+      {WGPUFeatureName_SharedTextureMemoryZirconHandle, "WGPUFeatureName_SharedTextureMemoryZirconHandle"},
+      {WGPUFeatureName_SharedTextureMemoryDXGISharedHandle, "WGPUFeatureName_SharedTextureMemoryDXGISharedHandle"},
+      {WGPUFeatureName_SharedTextureMemoryD3D11Texture2D, "WGPUFeatureName_SharedTextureMemoryD3D11Texture2D"},
+      {WGPUFeatureName_SharedTextureMemoryIOSurface, "WGPUFeatureName_SharedTextureMemoryIOSurface"},
+      {WGPUFeatureName_SharedTextureMemoryEGLImage, "WGPUFeatureName_SharedTextureMemoryEGLImage"},
+      {WGPUFeatureName_SharedFenceVkSemaphoreOpaqueFD, "WGPUFeatureName_SharedFenceVkSemaphoreOpaqueFD"},
+      {WGPUFeatureName_SharedFenceSyncFD, "WGPUFeatureName_SharedFenceSyncFD"},
+      {WGPUFeatureName_SharedFenceVkSemaphoreZirconHandle, "WGPUFeatureName_SharedFenceVkSemaphoreZirconHandle"},
+      {WGPUFeatureName_SharedFenceDXGISharedHandle, "WGPUFeatureName_SharedFenceDXGISharedHandle"},
+      {WGPUFeatureName_SharedFenceMTLSharedEvent, "WGPUFeatureName_SharedFenceMTLSharedEvent"},
+      {WGPUFeatureName_SharedBufferMemoryD3D12Resource, "WGPUFeatureName_SharedBufferMemoryD3D12Resource"},
+      {WGPUFeatureName_StaticSamplers, "WGPUFeatureName_StaticSamplers"},
+      {WGPUFeatureName_YCbCrVulkanSamplers, "WGPUFeatureName_YCbCrVulkanSamplers"},
+      {WGPUFeatureName_ShaderModuleCompilationOptions, "WGPUFeatureName_ShaderModuleCompilationOptions"},
+      {WGPUFeatureName_DawnLoadResolveTexture, "WGPUFeatureName_DawnLoadResolveTexture"},
+      {WGPUFeatureName_DawnPartialLoadResolveTexture, "WGPUFeatureName_DawnPartialLoadResolveTexture"},
+      {WGPUFeatureName_MultiDrawIndirect, "WGPUFeatureName_MultiDrawIndirect"},
+      {WGPUFeatureName_DawnTexelCopyBufferRowAlignment, "WGPUFeatureName_DawnTexelCopyBufferRowAlignment"},
+      {WGPUFeatureName_FlexibleTextureViews, "WGPUFeatureName_FlexibleTextureViews"},
+      {WGPUFeatureName_ChromiumExperimentalSubgroupMatrix, "WGPUFeatureName_ChromiumExperimentalSubgroupMatrix"},
+      {WGPUFeatureName_SharedFenceEGLSync, "WGPUFeatureName_SharedFenceEGLSync"},
+      {WGPUFeatureName_DawnDeviceAllocatorControl, "WGPUFeatureName_DawnDeviceAllocatorControl"},
+      {WGPUFeatureName_AdapterPropertiesWGPU, "WGPUFeatureName_AdapterPropertiesWGPU"},
+      {WGPUFeatureName_SharedBufferMemoryD3D12SharedMemoryFileMappingHandle, "WGPUFeatureName_SharedBufferMemoryD3D12SharedMemoryFileMappingHandle"},
+      {WGPUFeatureName_SharedTextureMemoryD3D12Resource, "WGPUFeatureName_SharedTextureMemoryD3D12Resource"},
+      {WGPUFeatureName_ChromiumExperimentalSamplingResourceTable, "WGPUFeatureName_ChromiumExperimentalSamplingResourceTable"},
+      {WGPUFeatureName_Force32, "WGPUFeatureName_Force32"},
+#endif // not __EMSCRIPTEN__
+  };
+
+  WGPUSupportedFeatures supportedFeatures;
+  wgpuAdapterGetFeatures(adapter, &supportedFeatures);
+
+  std::cout << "Supported features:" << std::endl;
+  for (size_t i = 0; i < supportedFeatures.featureCount; i++)
+  {
+    std::cout << " - ";
+    auto it = featureToString.find(supportedFeatures.features[i]);
+    if (it == featureToString.end())
+    {
+      std::cout << "Unknown feature: " << supportedFeatures.features[i] << std::endl;
+      ;
+    }
+    else
+    {
+      std::cout << it->second << std::endl;
+    }
+  }
+
   wgpuInstanceRelease(instance);
   return 0;
 }
