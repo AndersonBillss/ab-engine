28daed665d2f3da7120b634393fd559c76a772ca
AndersonBillss <anderson@thebillsfamily.com>
2026-02-08 00:59:17 -0700

    Implement run on CanvasWindow

--- src/window/canvasWindow.cpp
+++ src/window/canvasWindow.cpp
@@ -1,4 +1,5 @@
 #include "canvasWindow.hpp"
+#include <emscripten/emscripten.h>
 
 CanvasWindow::~CanvasWindow()
 {
@@ -13,18 +14,27 @@ CanvasWindow::CanvasWindow(int width, int height, std::string title) : Window(wi
 
 CanvasWindow::CanvasWindow(std::string title) : CanvasWindow(default_width, default_height, title) {}
 
-void CanvasWindow::setOnExit(Window::ExitCallback cb) {
+void CanvasWindow::setOnTick(Window::TickCallback cb)
+{
+    this->_onTick = cb;
+}
+
+void CanvasWindow::setOnExit(Window::ExitCallback cb)
+{
     this->_onExit = cb;
 }
 
 void CanvasWindow::run()
 {
-    this->_onTick(0);
-    this->_onExit();
-}
-
-void CanvasWindow::setOnTick(Window::TickCallback cb) {
-    this->_onTick = cb;
+    em_arg_callback_func cb = [](void *data)
+    {
+        auto *self = static_cast<CanvasWindow *>(data);
+        std::chrono::time_point now = std::chrono::steady_clock::now();
+        double dtSeconds = std::chrono::duration_cast<std::chrono::duration<double>>(now - self->_lastFrame).count();
+        self->_lastFrame = now;
+        self->_onTick(dtSeconds);
+    };
+    emscripten_set_main_loop_arg(cb, this, 0, 1);
 }
 
 bool CanvasWindow::isInitialized()
--- src/window/canvasWindow.hpp
+++ src/window/canvasWindow.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "window.hpp"
 #include <string>
+#include <chrono>
 
 class CanvasWindow : public Window
 {
@@ -8,12 +9,14 @@ public:
     ~CanvasWindow() override;
     CanvasWindow(int width, int height, std::string title);
     CanvasWindow(std::string title);
-    
+
     void setOnTick(Window::TickCallback cb) override;
     void setOnExit(Window::ExitCallback cb) override;
     void run() override;
     bool isInitialized() override;
+
 private:
     Window::TickCallback _onTick;
     Window::ExitCallback _onExit;
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration> _lastFrame;
 };
